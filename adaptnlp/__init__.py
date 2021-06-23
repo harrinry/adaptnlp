c1ef31e5c119da2e017cbacaf028522d09f952a8
Zach Mueller <muellerzr@gmail.com>
2021-06-23 18:11:05 +0000

    Start of training API, remove old

--- adaptnlp/__init__.py
+++ adaptnlp/__init__.py
@@ -1,28 +1,32 @@
+__version__ = "0.2.3"
+
 import pkg_resources
 from pathlib import Path
 
 # Easy Modules
-from .embeddings import *
-from .token_classification import *
-from .sequence_classification import *
-from .question_answering import *
-from .summarization import *
-from .translation import *
-from .text_generation import *
-from .language_model import *
-from .model_hub import *
+from .embeddings import (
+    EasyWordEmbeddings,
+    EasyStackedEmbeddings,
+    EasyDocumentEmbeddings,
+)
+from .token_classification import EasyTokenTagger
+from .sequence_classification import (
+    EasySequenceClassifier,
+    TransformersSequenceClassifier,
+    FlairSequenceClassifier,
+)
+from .question_answering import EasyQuestionAnswering, TransformersQuestionAnswering
+from .summarization import EasySummarizer, TransformersSummarizer
+from .translation import EasyTranslator, TransformersTranslator
+from .text_generation import EasyTextGenerator, TransformersTextGenerator
 
-# Training and Fine-tuning Modules
-# TODO: Deprecating in 0.3.0+
-from .training import SequenceClassifierTrainer
-from .transformers.finetuning import LMFineTunerManual
 
 # global variable like flair's: cache_root
 cache_root = Path.home()/".adaptnlp"
 
-__version__ = "0.2.3"
 
 __all__ = [
+    "__version__",
     "EasyWordEmbeddings",
     "EasyStackedEmbeddings",
     "EasyDocumentEmbeddings",
